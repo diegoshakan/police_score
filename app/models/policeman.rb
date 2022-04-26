14075e81772b6d035c027f8b30bcc82f030a8c60
Diego Araujo <diego_shakan@hotmail.com>
2022-04-25 21:30:49 -0300

    feat(policeman): enum to positions model and view

--- app/models/policeman.rb
+++ app/models/policeman.rb
@@ -7,6 +7,24 @@ class Policeman < ApplicationRecord
 
   scope :last_month_with_occurrence, -> { includes(:occurrences).where(occurrences: { date: (Time.current.beginning_of_month - 1.month)..(Time.current.end_of_month - 1.month) }) }
 
+  enum position: {
+    "Aluno Soldado": "Aluno Soldado",
+    "Soldado": "Soldado",
+    "Cabo": "Cabo",
+    "Aluno Sargento": "Aluno Sargento",
+    "3º Sargento": "3º Sargento",
+    "2º Sargento": "2º Sargento",
+    "1º Sargento": "1º Sargento",
+    "Subtenente": "Subtenente",
+    "Aspirante": "Aspirante",
+    "2º Tenente": "2º Tenente",
+    "1º Tenente": "1º Tenente",
+    "Capitão": "Capitão",
+    "Major": "Major",
+    "Tenente-Coronel": "Tenente-Coronel",
+    "Coronel": "Coronel"
+  }
+
   def total_score
     Policemen::TotalScore.new(self).call
   end
