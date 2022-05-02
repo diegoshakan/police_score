fff8ee70efeed0e779818ebf75b958bc0408e59e
Diego Araujo <diego_shakan@hotmail.com>
2022-05-02 20:45:16 -0300

    chore(last_month): sort by score

--- app/controllers/policemen/last_month_controller.rb
+++ app/controllers/policemen/last_month_controller.rb
@@ -1,5 +1,5 @@
 class Policemen::LastMonthController < ApplicationController
   def index
-    @policemen = Policeman.last_month_with_occurrence.limit(5)
+    @policemen = Policeman.last_month_with_occurrence.limit(5).sort_by(&:score_current_month).reverse
   end
 end
